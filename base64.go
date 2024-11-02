1855946089442a86798df092679caea7b1b3d4d3
Vladimir Pronin <vladimir.pronin@lamoda.ru>
2024-11-02 22:30:19 +0300

    add decode func

--- base64.go
+++ base64.go
@@ -1,8 +1,13 @@
 package base64
 
+import (
+	"strings"
+)
+
 const mapper = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 const symbolZeroByte = "="
 
+// Encode Кодирование строки в base64
 func Encode(str string) string {
 	var result string
 
@@ -10,12 +15,12 @@ func Encode(str string) string {
 	bytes := []byte(str)
 
 	// Строка в виде байт в чанках по 3
-	splitBytes := splitBytesTo3ByteChunk(bytes)
+	splitBytes := splitBytesToByteChunk(bytes, 3)
 
 	// Длина массива байт с чанками
 	lenSplitBytes := len(splitBytes)
 
-	// Количество добавленных 0 байт
+	// Количество добавленных нулевых байт
 	countZeroBytes := lenSplitBytes*3 - len(bytes)
 
 	for i := 0; i < lenSplitBytes; i++ {
@@ -31,18 +36,70 @@ func Encode(str string) string {
 	return result
 }
 
-// Разбивка массива байт на чанки по 3 байта
-func splitBytesTo3ByteChunk(bytes []byte) [][3]byte {
-	var result [][3]byte
+// Decode Декодирование base64 в строку
+func Decode(str string) string {
+	var result string
+
+	// Удаляем символы нулевого байта из конца строки
+	str = trimSymbolZeroByte(str)
+
+	// Слайс байт с индексами mapper
+	var indexBytes []byte
+
+	for i := 0; i < len(str); i++ {
+		// Индекс строки из mapper
+		index := strings.Index(mapper, string(str[i]))
+		indexBytes = append(indexBytes, byte(index))
+	}
+
+	// Разбиваем байты на чанки по 4
+	splitBytes := splitBytesToByteChunk(indexBytes, 4)
+
+	// Длина массива байт с чанками
+	lenSplitBytes := len(splitBytes)
+
+	// Количество добавленных нулевых байт
+	countZeroBytes := lenSplitBytes*4 - len(indexBytes)
+
+	for _, a := range splitBytes {
+		// Получаем чанки по 3 байта с символами utf-8
+		utfBytes := transform4BytesTo3Bytes(a)
+
+		result += string(utfBytes)
+	}
+
+	// Возвращаем слайс без нулевых байт
+	return result[:len(result)-countZeroBytes]
+}
+
+// Удаление символа нулевого байта из конца строки
+func trimSymbolZeroByte(str string) string {
+	var result string
+
+	for i := len(str) - 1; i >= 0; i-- {
+		if str[i] == symbolZeroByte[0] {
+			continue
+		} else {
+			result += str[:i+1]
+			break
+		}
+	}
+
+	return result
+}
+
+// Разбивка массива байт на чанки по n байт
+func splitBytesToByteChunk(bytes []byte, n int) [][]byte {
+	var result [][]byte
 
 	j := 0
 
-	var chunk [3]byte
+	chunk := make([]byte, n)
 
 	for i := 0; i < len(bytes); i++ {
-		if i%3 == 0 && i != 0 {
+		if i%n == 0 && i != 0 {
 			result = append(result, chunk)
-			chunk = [3]byte{0, 0, 0}
+			chunk = make([]byte, n)
 			j = 0
 		}
 
@@ -67,8 +124,8 @@ func convertBytesToString(bytes []byte, countZeroBytes int) (result string) {
 	return result
 }
 
-// Трансформация буффера из 3 байтов в буффер из 4 байтов
-func transform3BytesTo4Bytes(bytes [3]byte) []byte {
+// Трансформация буфера из 3 байтов в буфер из 4 байтов
+func transform3BytesTo4Bytes(bytes []byte) []byte {
 	b1 := bytes[0]
 	b2 := bytes[1]
 	b3 := bytes[2]
@@ -79,17 +136,34 @@ func transform3BytesTo4Bytes(bytes [3]byte) []byte {
 	result[0] = extractBites(b1, 2, 7)
 
 	// 2 результирующий байт получаем путем сложения 2 младших бит 1 байта и 4 старших бит 2 байта
-	result[1] = byte(0)
-	mask2 := byte(0b00000011)
-	result[1] = (b1&mask2)<<4 | b2>>4
+	result[1] = extractBites(b1, 0, 1)<<4 | extractBites(b2, 4, 7)
 
 	// 3 результирующий байт получаем путем сложения 4 младших бит 2 байта и 2 старших бит 3 байта
-	result[2] = byte(0)
-	mask3 := byte(0b00001111)
-	result[2] = (b2&mask3)<<2 | b3>>6
+	result[2] = extractBites(b2, 0, 3)<<2 | extractBites(b3, 6, 7)
 
 	// 4 результирующий байт получаем из 6 младших бит 3 байта
 	result[3] = extractBites(b3, 0, 5)
+
+	return result
+}
+
+// Трансформация буффера из 4 байтов в буффер из 3 байтов
+func transform4BytesTo3Bytes(bytes []byte) []byte {
+	b1 := bytes[0]
+	b2 := bytes[1]
+	b3 := bytes[2]
+	b4 := bytes[3]
+
+	result := make([]byte, 3)
+
+	// 1 результирующий байт получаем путем сложения 6 младших бит 1 байта и 4-5 бит 2 байта
+	result[0] = b1<<2 | extractBites(b2, 4, 5)
+
+	// 2 результирующий байт получаем путем сложения 4 младших бит 2 байта и 2-5 бит 3 байта
+	result[1] = b2<<4 | extractBites(b3, 2, 5)
+
+	// 3 результирующий байт получаем путем сложения 2 младших бит 3 байта и 6 младших бит 4 байта
+	result[2] = b3<<6 | extractBites(b4, 0, 5)
 
 	return result
 }
--- base64_test.go
+++ base64_test.go
@@ -25,10 +25,47 @@ func TestEncode(t *testing.T) {
 			input:    "one",
 			expected: "b25l",
 		},
+		{
+			input:    "привет",
+			expected: "0L/RgNC40LLQtdGC",
+		},
 	}
 
 	for _, data := range testData {
 		result := Encode(data.input)
+
+		if result != data.expected {
+			t.Errorf("input: %s, expected %s, got %s", data.input, data.expected, result)
+		}
+	}
+}
+
+func TestDecode(t *testing.T) {
+	testData := []TestData{
+		{
+			input:    "aGVsbG8=",
+			expected: "hello",
+		},
+		{
+			input:    "d29ybGQ=",
+			expected: "world",
+		},
+		{
+			input:    "aGVsbG8sIHdvcmxkIQ==",
+			expected: "hello, world!",
+		},
+		{
+			input:    "b25l",
+			expected: "one",
+		},
+		{
+			input:    "0L/RgNC40LLQtdGC",
+			expected: "привет",
+		},
+	}
+
+	for _, data := range testData {
+		result := Decode(data.input)
 
 		if result != data.expected {
 			t.Errorf("input: %s, expected %s, got %s", data.input, data.expected, result)
